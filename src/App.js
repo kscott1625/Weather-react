8b0d2d92b69fcfc1de37396564b4f9ea86b6cf9c
Katherine Scott <kasc1625@gmail.com>
2022-02-17 17:18:01 -0800

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+const api = {
+  key: '056d803b7fda4d93889c479f4278b0ee',
+  base: 'https://api.openweathermap.org/data/2.5/'
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="search.." />
+        </div>
+      </main>
     </div>
   );
 }
