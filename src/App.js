4eaa272c3b136ac8f65b63f0a24debcfc2c5f0f0
Katherine Scott <kasc1625@gmail.com>
2022-02-19 16:33:37 -0800

    placement

--- src/App.js
+++ src/App.js
@@ -5,11 +5,52 @@ const api = {
 };
 
 function App() {
+  const dateBuilder = (d) => {
+    let months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];
+    let days = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday'
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
   return (
     <div className="App">
       <main>
         <div className="search-box">
           <input type="text" className="search-bar" placeholder="search.." />
+        </div>
+        <div className="location-box">
+          <div className="location">Redmond,WA</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">50°F</div>
+          <dov className="weather">Sunny</dov>
         </div>
       </main>
     </div>
